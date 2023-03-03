c0a49144eb27d5ed42e849ea49078171b90fcf92
Divo <aryadivo02@gmail.com>
2023-03-03 01:11:27 +0100

    created page for list of quests

--- app/src/main/java/com/example/geodetective/ListOfQuests.java
+++ app/src/main/java/com/example/geodetective/ListOfQuests.java
@@ -5,15 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 
 public class ListOfQuests extends AppCompatActivity {
+    String quests[] = {"Meta-Quest", "Audi-Quest", "Aurora-Quest"};
+    int questsimage[] = {R.drawable.metaforum, R.drawable.auditorium, R.drawable.aurora};
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_quests);
-
-
+        listView = (ListView) findViewById(R.id.customListView);
+        CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(getApplicationContext(), quests, questsimage);
+        listView.setAdapter(customBaseAdapter);
     }
 }
