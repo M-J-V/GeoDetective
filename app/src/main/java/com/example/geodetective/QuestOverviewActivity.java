cfda86921b484ae1ef109c5eded66e217bff3067
Gijs Nolet <54958945+Gizzle1@users.noreply.github.com>
2023-03-07 20:43:22 +0100

    Created the join quest interface

--- app/src/main/java/com/example/geodetective/QuestOverviewActivity.java
+++ app/src/main/java/com/example/geodetective/QuestOverviewActivity.java
@@ -1,6 +1,10 @@
 package com.example.geodetective;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +14,18 @@ public class QuestOverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest_overview);
+
+        // Get buttons from activity
+        Button startQuestButton = findViewById(R.id.join_quest_btn);
+        Button hintButton = findViewById(R.id.reveal_hint_btn);
+        ImageButton backButton = findViewById(R.id.BackBtn);
+
+        // Get ImageView from activity
+        ImageView bitmap = findViewById(R.id.Quest_Image);
+
+        // Get text fields from activity
+        TextView imageClue = findViewById(R.id.image_hint);
+        TextView questDescription = findViewById(R.id.quest_description);
     }
 
 }
