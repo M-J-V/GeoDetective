0a86bf43e09bfab5e6f5855ee558bc87144ebb14
Gijs Nolet <54958945+Gizzle1@users.noreply.github.com>
2023-03-07 18:18:24 +0100

    Added GPS data to quest creation

--- app/src/main/java/com/example/geodetective/CreateQuestActivity.java
+++ app/src/main/java/com/example/geodetective/CreateQuestActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
@@ -15,7 +16,9 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -24,7 +27,14 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,9 +43,11 @@ import java.util.Date;
 
 public class CreateQuestActivity extends AppCompatActivity {
 
-    private ImageView questImage;
     private static final int CAMERA_REQUEST = 1888;
     private static final int SELECT_PICTURE = 200;
+    private ImageView questImage;
+    private double longitude = 0;
+    private double latitude = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,22 +60,68 @@ public class CreateQuestActivity extends AppCompatActivity {
         // Get buttons from activity
         Button chooseImageBtn = findViewById(R.id.choose_Quest_Image_Btn);
         Button submitQuestBtn = findViewById(R.id.submit_quest_btn);
+        ImageButton backBtn = findViewById(R.id.BackBtn);
 
         // get text inputs from activity
         EditText questName = findViewById(R.id.quest_name_input);
         EditText questDescription = findViewById(R.id.quest_description_input);
         EditText questHint = findViewById(R.id.quest_hint_input);
 
+        // Set back button functionality
+        backBtn.setOnClickListener(v -> {
+            // return to home activity
+            finish();
+        });
+
+        // Select image from gallery or take a photo.
         chooseImageBtn.setOnClickListener(v -> selectImage());
 
-        Quest quest = new Quest(getBitmapFromDrawable(questImage.getDrawable()), questName.getText().toString(), questDescription.getText().toString(), questHint.getText().toString());
+        //Get current location
+        updateLocation();
 
         //TODO save quest to database
-        submitQuestBtn.setOnClickListener(view -> {
-            //Database.saveQuest();
+//        submitQuestBtn.setOnClickListener(view -> {
+//            Database.saveQuest(creatorName,
+//                    longitude,
+//                    latitude,
+//                    questName.getText().toString(),
+//                    questDescription.getText().toString(),
+//                    questHint.getText().toString(),
+//                    getBitmapFromDrawable(questImage.getDrawable()));
+//        });
+    }
+
+    private void updateLocation() {
+        //Create location client
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //Check if permission is granted
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Send permissions request
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        //Get current location
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, new CancellationToken() {
+            @SuppressWarnings("ConstantConditions")
+            @NonNull
+            @Override
+            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
+                return null;
+            }
+
+            @Override
+            public boolean isCancellationRequested() {
+                return false;
+            }
+        }).addOnSuccessListener(location -> {
+            //Set longitude and latitude
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
         });
     }
 
+    // Select image from gallery or take a photo.
     private void selectImage() {
         final CharSequence[] options = { "Take Photo", "Choose from Gallery"};
         AlertDialog.Builder builder = new AlertDialog.Builder(CreateQuestActivity.this);
@@ -92,6 +150,7 @@ public class CreateQuestActivity extends AppCompatActivity {
         builder.show();
     }
 
+    // Get image from camera or gallery
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
@@ -111,6 +170,7 @@ public class CreateQuestActivity extends AppCompatActivity {
         }
     }
 
+    // Convert drawable to bitmap
     @NonNull
     static private Bitmap getBitmapFromDrawable(@NonNull Drawable drawable) {
         final Bitmap bmp = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
@@ -119,4 +179,25 @@ public class CreateQuestActivity extends AppCompatActivity {
         drawable.draw(canvas);
         return bmp;
     }
+
+    // Handle permission request result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // permission not granted, show a message to the user and disable the feature
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Permission denied");
+                builder.setMessage("You need to grant location permission to use this feature, please grant it in the settings.");
+                builder.setOnDismissListener(dialog -> {
+                    // Call finish() after the dialog is dismissed
+                    finish();
+                });
+                builder.show();
+            }
+
+        }
+    }
+
 }
