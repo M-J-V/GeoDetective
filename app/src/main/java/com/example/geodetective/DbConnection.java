36a4f305c58afc774986518bf4578c53dfff31f0
M-J-V <malcolmvas@gmail.com>
2023-03-09 01:25:05 +0100

    Database connection for register and login

---
+++ app/src/main/java/com/example/geodetective/DbConnection.java
@@ -0,0 +1,53 @@
+package com.example.geodetective;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A singleton class that is used to connect and interface with the firebase database.
+ */
+public class DbConnection {
+
+    private static DbConnection connection = null;
+    FirebaseFirestore db;
+    CollectionReference users;
+    CollectionReference quests;
+    CollectionReference attempted;
+
+    private DbConnection() {
+        this.db = FirebaseFirestore.getInstance();
+        this.users = db.collection("Users");
+        this.quests = db.collection("Quests");
+        this.attempted = db.collection("Attempted");
+    }
+
+    /**
+     * A static method that returns the instance of the class.
+     *
+     * @return an instance of the singleton class.
+     */
+    public static DbConnection getInstance() {
+        if (connection == null) {
+            connection = new DbConnection();
+        }
+
+        return connection;
+    }
+
+}
