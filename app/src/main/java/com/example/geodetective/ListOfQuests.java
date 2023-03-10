d0ed8652f9808bbb6373f9334751f6db9d7ba467
gijsnolet06@gmail.com <FUzL&un23^OJ9bG8>
2023-03-10 14:39:05 +0100

    Removed all android studio warnings

--- app/src/main/java/com/example/geodetective/ListOfQuests.java
+++ app/src/main/java/com/example/geodetective/ListOfQuests.java
@@ -1,67 +1,60 @@
 package com.example.geodetective;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class ListOfQuests extends AppCompatActivity {
-    String quests[] = {"Meta-Quest", "Audi-Quest", "Aurora-Quest"};
-    String authors[] = {"Author: John Smith", "Author: John Smith", "Author: John Smith"};
-    int questsimage[] = {R.drawable.metaforum, R.drawable.auditorium, R.drawable.aurora};
+    String[] quests = {"Meta-Quest", "Audi-Quest", "Aurora-Quest"};
+    String[] authors = {"Author: John Smith", "Author: John Smith", "Author: John Smith"};
+    int[] questImages = {R.drawable.metaforum, R.drawable.auditorium, R.drawable.aurora};
 
     ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_quests);
-        listView = (ListView) findViewById(R.id.customListView);
-        CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(getApplicationContext(), quests, authors, questsimage);
+        listView = findViewById(R.id.customListView);
+        CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(getApplicationContext(), quests, authors, questImages);
         listView.setAdapter(customBaseAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                //startActivity(new Intent(getApplicationContext(), QuestOverviewActivity.class));
+        listView.setOnItemClickListener((adapterView, view, position, l) -> {
+            //startActivity(new Intent(getApplicationContext(), QuestOverviewActivity.class));
 
-                if (position == 0) {
-                    String questName = "Meta-Quest";
-                    String questDescription = "Find the plane";
-                    Intent intent = new Intent (ListOfQuests.this, QuestOverviewActivity.class);
-                    intent.putExtra("questName", questName);
-                    intent.putExtra("questDescription", questDescription);
-                    intent.putExtra("questImage", R.drawable.metaforum);
-                    startActivity(intent);
-                }
-
-                if (position == 1) {
-                    String questName = "Audi-Quest";
-                    String questDescription = "Find the organ";
-                    Intent intent = new Intent (ListOfQuests.this, QuestOverviewActivity.class);
-                    intent.putExtra("questName", questName);
-                    intent.putExtra("questDescription", questDescription);
-                    intent.putExtra("questImage", R.drawable.auditorium);
-                    startActivity(intent);
-                }
-
-                if (position == 2) {
-                    String questName = "Aurora-Quest";
-                    String questDescription = "Find room 511";
-                    Intent intent = new Intent (ListOfQuests.this, QuestOverviewActivity.class);
-                    intent.putExtra("questName", questName);
-                    intent.putExtra("questDescription", questDescription);
-                    intent.putExtra("questImage", R.drawable.aurora);
-                    startActivity(intent);
-                }
-
-
+            if (position == 0) {
+                String questName = "Meta-Quest";
+                String questDescription = "Find the plane";
+                Intent intent = new Intent (ListOfQuests.this, QuestOverviewActivity.class);
+                intent.putExtra("questName", questName);
+                intent.putExtra("questDescription", questDescription);
+                intent.putExtra("questImage", R.drawable.metaforum);
+                startActivity(intent);
             }
+
+            if (position == 1) {
+                String questName = "Audi-Quest";
+                String questDescription = "Find the organ";
+                Intent intent = new Intent (ListOfQuests.this, QuestOverviewActivity.class);
+                intent.putExtra("questName", questName);
+                intent.putExtra("questDescription", questDescription);
+                intent.putExtra("questImage", R.drawable.auditorium);
+                startActivity(intent);
+            }
+
+            if (position == 2) {
+                String questName = "Aurora-Quest";
+                String questDescription = "Find room 511";
+                Intent intent = new Intent (ListOfQuests.this, QuestOverviewActivity.class);
+                intent.putExtra("questName", questName);
+                intent.putExtra("questDescription", questDescription);
+                intent.putExtra("questImage", R.drawable.aurora);
+                startActivity(intent);
+            }
+
+
         });
 
         //TODO start QuestOverviewActivity when a quest is clicked (and pass along some information like a quest ID to load the quest information)
