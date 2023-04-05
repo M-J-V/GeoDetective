e2673c38ae1e063591dab4277b35c94f2dfe11fa
gijsnolet06@gmail.com <FUzL&un23^OJ9bG8>
2023-04-05 16:39:44 +0200

    Added documentation and removed some comments

--- app/src/main/java/com/example/geodetective/gameComponents/ImageInput.java
+++ app/src/main/java/com/example/geodetective/gameComponents/ImageInput.java
@@ -15,6 +15,10 @@ import com.example.geodetective.singletons.UserPreferences;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+/**
+ * The ImageInput class handles selecting and compressing images from the camera or gallery, checking
+ * for necessary permissions, and displaying the selected image in an ImageView.
+ */
 public class ImageInput {
 
     private static final int CAMERA_REQUEST = 1888;
@@ -28,8 +32,15 @@ public class ImageInput {
         askPermissions(false);
     }
 
+    /**
+     * The function asks for camera and gallery permissions and stores the user's response in
+     * preferences.
+     *
+     * @param triedToUseResourceWithoutPermission A boolean value indicating whether the user has
+     * already tried to use a resource (such as the camera or gallery) without having the necessary
+     * permissions.
+     */
     private void askPermissions(boolean triedToUseResourceWithoutPermission) {
-        //TODO Not both permissions should have to be granted, but is required now!
         preferences = UserPreferences.getInstance(activity);
 
         boolean permissionsAsked = preferences.contains("cameraPermissions") && preferences.contains("galleryPermissions");
@@ -61,7 +72,10 @@ public class ImageInput {
         builder.show();
     }
 
-    // Select image from gallery or take a photo.
+    /**
+     * The function allows the user to select an image from either the camera or gallery, but first
+     * checks if the necessary permissions have been granted.
+     */
     public void selectImage() {
         preferences = UserPreferences.getInstance(activity);
 
@@ -99,7 +113,25 @@ public class ImageInput {
         });
         builder.show();
     }
-    // Get image from camera or gallery
+
+    /**
+     * This function handles the result of either taking a photo or selecting an image from the
+     * gallery, compresses the image, checks if the image size is too large, and sets the image to an
+     * ImageView if it is not too large.
+     *
+     * @param requestCode An integer code that identifies the type of request being made. It is used to
+     * distinguish between different types of requests when the result is returned.
+     * @param resultCode The result code returned by the activity that was started for result using
+     * startActivityForResult(). It indicates whether the operation was successful or not.
+     * @param data The Intent data parameter is the data returned from the activity launched for a
+     * result. It can contain various types of data, depending on the activity that was launched and
+     * the data that was requested. In this case, it is used to retrieve the image data captured by the
+     * camera or selected from the gallery.
+     * @param questImage An ImageView object that will display the selected image.
+     * @param context The context parameter is a reference to the current context of the application.
+     * It is used to access resources and services related to the application, such as Toast messages
+     * and the content resolver.
+     */
     public void onActivityResult(int requestCode, int resultCode, Intent data, ImageView questImage, Context context) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
