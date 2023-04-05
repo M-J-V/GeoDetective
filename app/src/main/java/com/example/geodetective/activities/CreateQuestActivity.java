87925cc6e1c5240f9265a1418c6862061476e8ee
gijsnolet06@gmail.com <FUzL&un23^OJ9bG8>
2023-04-05 16:23:54 +0200

    Added documentation to CreateQuestActivity

--- app/src/main/java/com/example/geodetective/activities/CreateQuestActivity.java
+++ app/src/main/java/com/example/geodetective/activities/CreateQuestActivity.java
@@ -16,17 +16,21 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.geodetective.singletons.ActiveUser;
-import com.example.geodetective.singletons.DbConnection;
+import com.example.geodetective.R;
 import com.example.geodetective.gameComponents.ImageInput;
 import com.example.geodetective.gameComponents.Location;
 import com.example.geodetective.gameComponents.Quest;
-import com.example.geodetective.R;
+import com.example.geodetective.singletons.ActiveUser;
+import com.example.geodetective.singletons.DbConnection;
 import com.google.firebase.firestore.DocumentSnapshot;
 
+/**
+ * This is a Java class for creating a quest in an Android app, which includes functionality for
+ * uploading a quest with a title, description, hint, image, and location.
+ */
 public class CreateQuestActivity extends AppCompatActivity {
-    DbConnection db = DbConnection.getInstance();
-    ActiveUser user = ActiveUser.getInstance();
+    private final DbConnection db = DbConnection.getInstance();
+    private final ActiveUser user = ActiveUser.getInstance();
     private Location location;
     private ImageInput imageInput;
     private ImageView questImage;
@@ -37,6 +41,15 @@ public class CreateQuestActivity extends AppCompatActivity {
 
     private Quest questUpload;
 
+    /**
+     * This function sets up the activity for creating a quest, including initializing various UI
+     * elements and setting their functionality.
+     *
+     * @param savedInstanceState savedInstanceState is a Bundle object that contains the activity's
+     * previously saved state. It is used to restore the activity's state when it is recreated, such as
+     * when the device is rotated or the activity is destroyed and recreated due to a configuration
+     * change.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,7 +89,10 @@ public class CreateQuestActivity extends AppCompatActivity {
         submitQuestBtn.setOnClickListener(view -> uploadQuest());
     }
 
-    //TODO authenticate that the quest is valid, title not already used, non empty desc
+    /**
+     * The function uploads a quest with a title, description, hint, creator, and image, after checking
+     * for valid input and updating the location.
+     */
     private void uploadQuest() {
         // update location again
         String title = questName.getText().toString().trim();
@@ -113,6 +129,13 @@ public class CreateQuestActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * This function adds a new quest to a database and checks if the quest title already exists.
+     *
+     * @param location The location parameter is an object of the Location class that represents the
+     * location of a quest being added. It is used to set the location of the questUpload object before
+     * checking if the quest title already exists in the database.
+     */
     public void addQuest(Location location) {
         questUpload.setLocation(location);
         db.quests.document(questUpload.getName()).get().addOnCompleteListener(task -> {
@@ -134,13 +157,41 @@ public class CreateQuestActivity extends AppCompatActivity {
         });
     }
 
-    // Get image from camera or gallery
+    /**
+     * This function calls the onActivityResult method of the imageInput object with the given
+     * parameters.
+     *
+     * @param requestCode requestCode is an integer value that is used to identify the request made by
+     * the calling activity. It is passed as a parameter to the startActivityForResult() method when
+     * the calling activity starts the target activity. The same value is returned to the calling
+     * activity when the target activity finishes and calls the setResult() method. This
+     * @param resultCode resultCode is an integer value that represents the result of the activity that
+     * was started for a result. It can have one of three values: RESULT_OK, RESULT_CANCELED, or any
+     * custom result code set by the activity that was started. RESULT_OK indicates that the activity
+     * completed successfully, RESULT_CANCELED indicates
+     * @param data The `data` parameter in the `onActivityResult` method is an `Intent` object that
+     * contains the result data returned from the activity launched by the `startActivityForResult`
+     * method. This data can include information such as the user's selected image, text input, or
+     * other data depending on the activity
+     */
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         imageInput.onActivityResult(requestCode, resultCode, data, questImage, getApplicationContext());
     }
 
-    // Handle permission request result
+    /**
+     * This function handles the result of a permission request for location access.
+     *
+     * @param requestCode An integer code that identifies the request for permission. This code is used
+     * to match the request with the corresponding result.
+     * @param permissions An array of permissions requested by the app. Each permission is represented
+     * as a string.
+     * @param grantResults grantResults is an integer array that contains the results of the permission
+     * requests. Each element in the array corresponds to the permission request at the same index in
+     * the permissions array. The value of the element can be either PackageManager.PERMISSION_GRANTED
+     * or PackageManager.PERMISSION_DENIED, depending on whether the user granted or denied the
+     * permission
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
